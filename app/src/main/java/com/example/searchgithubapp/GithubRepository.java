2bfc25d6a3aedd149e32d5afb2fe00de51d25694
ShubhiJug <shubhi.shrivastava779@gmail.com>
2022-04-30 17:50:20 +0530

    GitHub Repository Search App

---
+++ app/src/main/java/com/example/searchgithubapp/GithubRepository.java
@@ -0,0 +1,13 @@
+package com.example.searchgithubapp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@AllArgsConstructor
+
+@Data
+public class GithubRepository {
+    Integer id;
+    String name;
+    String description;
+
+}
